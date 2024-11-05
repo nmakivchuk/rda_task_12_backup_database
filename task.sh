80e50c78a996a9ab0a6b3969a48f67c3d8f2cba6
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-11-05 14:59:24 +0200

    'backup-db'

--- task.sh
+++ task.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+USER=$DB_USER
+PASSWORD=$DB_PASSWORD
+
+mysqldump -u "$USER" -p"$PASSWORD" ShopDB --no-create-db --result-file=backup-ShopDB.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBReserve < backup-ShopDB.sql
+
+mysqldump -u "$USER" -p"$PASSWORD" ShopDB --no-create-db --no-create-info --result-file=backup-no-data.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBDevelopment < backup-no-data.sql
